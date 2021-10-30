6060a2c1e8db4c7f097bceb0b37d86ffbe1a9dc8
Kingbond470 <mausamsingh470@gmail.com>
2021-10-30 20:37:31 +0530

    Initial Commit

---
+++ app/src/main/java/dev/kingbond/moveon/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.kingbond.moveon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
