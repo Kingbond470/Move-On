b8e127911f6a320c8f508e0265973efe6efc41cb
Kingbond470 <mausamsingh470@gmail.com>
2021-10-31 23:17:04 +0530

    Login Layout Done

---
+++ app/src/main/java/dev/kingbond/moveon/ui/login/Login.kt
@@ -0,0 +1,12 @@
+package dev.kingbond.moveon.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.kingbond.moveon.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+}
