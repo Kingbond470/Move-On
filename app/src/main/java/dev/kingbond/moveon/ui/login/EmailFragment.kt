21b58a17997c6046f93687ab89470adf4e9596d8
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-09 20:34:02 +0530

    Login Layout |  Done

--- app/src/main/java/dev/kingbond/moveon/ui/login/EmailFragment.kt
+++ app/src/main/java/dev/kingbond/moveon/ui/login/EmailFragment.kt
@@ -1,6 +1,7 @@
 package dev.kingbond.moveon.ui.login
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import dev.kingbond.moveon.R
 import kotlinx.android.synthetic.main.fragment_email.*
+import java.util.regex.Pattern
 
 class EmailFragment : Fragment(R.layout.fragment_email) {
 
@@ -22,24 +24,47 @@ class EmailFragment : Fragment(R.layout.fragment_email) {
 
 
         // Navigation
-       // findNavController().navigate(R.id.action_emailFragment_to_passwordFragment)
+        // findNavController().navigate(R.id.action_emailFragment_to_passwordFragment)
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnLoginWithEmailContinue.setOnClickListener {
-         //   findNavController().navigate(R.id.action_emailFragment_to_passwordFragment)
-            Navigation.findNavController(view).navigate(R.id.action_emailFragment_to_passwordFragment)
-        }
+
 
         tvLoginSignUp.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_emailFragment_to_signUpFragment)
         }
+
+        // if email is valid : continue to password
+        btnLoginWithEmailContinue.setOnClickListener {
+            var email = etLoginEmail.text.trim().toString()
+
+            if (isValidEmail(email)) {
+                Navigation.findNavController(view)
+                    .navigate(R.id.action_emailFragment_to_passwordFragment)
+
+            } else {
+                // Enter your valid email ID
+                etLoginEmail.error = "Enter valid email"
+            }
+        }
+
+
     }
 
 
+    fun isValidEmail(email: CharSequence): Boolean {
+        var isValid = true
+        val expression = "^[\\w.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
+        val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+        val matcher = pattern.matcher(email)
+        if (!matcher.matches()) {
+            isValid = false
+        }
+        return isValid
+    }
 
 
 }
--- app/src/main/java/dev/kingbond/moveon/ui/login/PasswordFragment.kt
+++ app/src/main/java/dev/kingbond/moveon/ui/login/PasswordFragment.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -28,11 +30,43 @@ class PasswordFragment : Fragment(R.layout.fragment_password) {
         super.onViewCreated(view, savedInstanceState)
 
 
+        // Go to Main/Home Page
         btnLoginWithPasswordContinue.setOnClickListener {
-            val intent=Intent(context,MainActivity::class.java)
-            startActivity(intent)
-            clickListener.onClick()
+
+            val password = etLoginPassword.text.trim().toString()
+
+            // if length of password is greater than 8 : good password
+            if (password.length >= 8) {
+
+                val intent = Intent(context, MainActivity::class.java)
+                startActivity(intent)
+                clickListener.onClick()
+
+            } else {
+                etLoginPassword.error = "Length must be 8 characters"
+            }
         }
+
+
+        // to show and hide the password
+        etLoginPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+        btnShowHideLogin.setOnClickListener {
+            if (etLoginPassword.transformationMethod == PasswordTransformationMethod.getInstance()) {
+                etLoginPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                btnShowHideLogin.setBackgroundResource(R.drawable.password_show)
+
+                //placing cursor at the end of the text
+                etLoginPassword.setSelection(etLoginPassword.text.toString().length)
+            } else {
+                etLoginPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                btnShowHideLogin.setBackgroundResource(R.drawable.password_hide)
+
+                //placing cursor at the end of the text
+                etLoginPassword.setSelection(etLoginPassword.text.toString().length)
+            }
+
+        }
+
     }
 
 
