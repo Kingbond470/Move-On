4014e54dbbddd2d6e726d4ebde9a69889f008095
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-11 17:33:44 +0530

    About, Settings and About Addition in DrawerLayout

--- app/src/main/java/dev/kingbond/moveon/MainActivity.kt
+++ app/src/main/java/dev/kingbond/moveon/MainActivity.kt
@@ -6,17 +6,23 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
+import dev.kingbond.moveon.ui.about.AboutFragment
+import dev.kingbond.moveon.ui.help.HelpFragment
+import dev.kingbond.moveon.ui.settings.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.navView)
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
@@ -29,24 +35,37 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
 
-            when (it.itemId) {
-                R.id.nav_your_bookings -> Toast.makeText(
-                    applicationContext,
-                    "Clicked  Your Bookings",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.nav_payment -> Toast.makeText(
-                    applicationContext,
-                    "Clicked  Your Payments",
-                    Toast.LENGTH_SHORT
-                ).show()
+            it.isChecked = true
 
+            when (it.itemId) {
+                R.id.nav_settings ->
+                    replaceFragment(SettingsFragment(), it.title.toString())
+
+                R.id.nav_about ->
+                    replaceFragment(AboutFragment(), it.title.toString())
+
+                R.id.nav_help ->
+                    replaceFragment(HelpFragment(), it.title.toString())
 
             }
             true
 
         }
 
+
+    }
+
+
+    private fun replaceFragment(fragment: Fragment, title: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.framelayout, fragment)
+        fragmentTransaction.commit()
+
+        // to close the drawer
+        drawerLayout.closeDrawers()
+        // to set the title on top
+        setTitle(title)
 
     }
 
