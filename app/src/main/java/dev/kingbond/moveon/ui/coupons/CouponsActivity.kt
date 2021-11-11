8f7b13695aa2a1a17db946e9a9a6ef128426ad6a
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-11 20:25:13 +0530

    Coupons Layout | Initial

---
+++ app/src/main/java/dev/kingbond/moveon/ui/coupons/CouponsActivity.kt
@@ -0,0 +1,12 @@
+package dev.kingbond.moveon.ui.coupons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.kingbond.moveon.R
+
+class CouponsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coupons)
+    }
+}
