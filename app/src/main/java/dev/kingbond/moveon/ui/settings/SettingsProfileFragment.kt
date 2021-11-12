01671fa0a785630026cf2623eccf4823f0ed8feb
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-12 21:31:03 +0530

    Setting Edit Profile | Logout

--- app/src/main/java/dev/kingbond/moveon/ui/settings/SettingsProfileFragment.kt
+++ app/src/main/java/dev/kingbond/moveon/ui/settings/SettingsProfileFragment.kt
@@ -1,13 +1,41 @@
 package dev.kingbond.moveon.ui.settings
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import dev.kingbond.moveon.R
+import dev.kingbond.moveon.ui.login.Login
+import kotlinx.android.synthetic.main.fragment_settings_profile.*
 
 
 class SettingsProfileFragment : Fragment(R.layout.fragment_settings_profile) {
+
+    //profile
+    private lateinit var mAuth: FirebaseAuth
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //profile
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+        if (user != null) {
+            Glide.with(ivEditProfileImage).load(user.photoUrl).into(ivEditProfileImage)
+            tvEditProfileName.text = user.displayName
+        }
+
+        tvEditLogout.setOnClickListener {
+            mAuth.signOut()
+            val logout = Intent(activity, Login::class.java)
+            startActivity(logout)
+            (activity as Activity?)?.overridePendingTransition(0, 0)
+        }
+
+    }
 
 }
