0a36e402f5dc02e78faa8cdf16a433ad1f812a23
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-12 12:59:48 +0530

    Coin Layout | Done

--- app/src/main/java/dev/kingbond/moveon/ui/coupons/CouponsActivity.kt
+++ app/src/main/java/dev/kingbond/moveon/ui/coupons/CouponsActivity.kt
@@ -60,12 +60,12 @@ class CouponsActivity : AppCompatActivity() {
     }
 
     private fun launchCoinFragment() {
-        wallet_title.text = "11 Coins Available"
+        //  wallet_title.text = "100 Coins Available"
+        wallet_title.text = "12 Coupons Available"
         wallet_title.setTextColor(0xFFFB8C00.toInt())
         val fragmentTransaction = fragmentManager.beginTransaction()
         val coinFragment = CoinFragment()
         fragmentTransaction.replace(R.id.wallet_fragment, coinFragment).commit()
-
 
     }
 
