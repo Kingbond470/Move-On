5c3b4f6d0bd33c86b697ee61a53cc03eb72a1acb
umangsh28 <im.mrstark08@gmail.com>
2021-11-13 02:26:12 +0530

    packers complete, payment complete

---
+++ app/src/main/java/dev/kingbond/moveon/ui/packageandmovers/spref/GlobalV.kt
@@ -0,0 +1,13 @@
+package dev.kingbond.moveon.ui.packageandmovers.spref
+
+public class GlobalV {
+
+    var rk1:String = "1 rk Standard"
+    var bhk1:String="1 bhk Standard"
+    val bhk2:String="2 bhk Standard"
+    val bhk3:String="3 bhk Standard"
+
+
+
+
+}
